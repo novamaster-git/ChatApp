bde4adca6b435d3b6057fcd2b851d888e5324c73
soumen.samanta <soumen.samanta@weavers-web.com>
2023-12-05 18:44:36 +0530

    ChatRoom UI created

---
+++ app/components/ChatHeader.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {wp} from '../utils/responsive.util';
+import BackIcon from '../assets/images/svg/back.svg';
+import {useNavigation} from '@react-navigation/native';
+export type ChatHeaderProps = {
+  name: string;
+  imageUrl: string;
+};
+function ChatHeader({name, imageUrl}: ChatHeaderProps): JSX.Element {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.headerContainer}>
+      <TouchableOpacity
+        style={styles.backButtonContainer}
+        onPress={() => navigation.goBack()}>
+        <BackIcon height={wp(8)} width={wp(8)} />
+      </TouchableOpacity>
+      <View style={styles.imageContainer}>
+        <Image
+          source={{uri: imageUrl ?? 'https://i.pravatar.cc/500'}}
+          style={styles.avatar}
+        />
+      </View>
+      <View style={styles.nameContainer}>
+        <Text style={styles.nameText}>{name ?? 'null'}</Text>
+      </View>
+    </View>
+  );
+}
+export default ChatHeader;
+const styles = StyleSheet.create({
+  headerContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    paddingVertical: wp(2),
+    borderBottomWidth: 0.5,
+    borderBottomColor: '#BDBDBD',
+    backgroundColor: '#F5F5F5',
+  },
+  backButtonContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  nameContainer: {
+    flex: 6,
+    paddingLeft: wp(2),
+    justifyContent: 'center',
+  },
+  avatar: {
+    width: wp(12),
+    height: wp(12),
+    borderRadius: wp(7),
+  },
+  nameText: {
+    color: 'black',
+    fontSize: wp(5),
+    fontWeight: '800',
+  },
+});
--- app/screens/ChatRoom/index.tsx
+++ app/screens/ChatRoom/index.tsx
@@ -1,10 +1,63 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {wp} from '../../utils/responsive.util';
+import ChatHeader from '../../components/ChatHeader';
+import SendIcon from '../../assets/images/svg/sent.svg';
+import BlankSpacer from '../../components/BlankSpacer';
 function ChatRoom() {
   return (
-    <View>
-      <Text>ChatRoom</Text>
+    <View style={styles.container}>
+      <ChatHeader name="Soumen" imageUrl="https://i.pravatar.cc/500" />
+      <View style={styles.container}>
+        <FlatList
+          data={[1, 2, 3, 4, 5, 6]}
+          renderItem={() => {
+            return <Text>Hello</Text>;
+          }}
+        />
+      </View>
+      <View style={styles.chatPadContainer}>
+        <TextInput style={styles.chatInput} placeholder="Enter New Message" />
+        <BlankSpacer width={wp(2)} />
+        <TouchableOpacity style={styles.sendButtonContainer}>
+          <SendIcon width={wp(10)} height={wp(10)} color="white" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 export default ChatRoom;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  chatPadContainer: {
+    width: '100%',
+    paddingVertical: wp(2),
+    paddingHorizontal: wp(3),
+    backgroundColor: '#F5F5F5',
+    flexDirection: 'row',
+  },
+  chatInput: {
+    fontSize: wp(4),
+    backgroundColor: 'white',
+    flex: 6,
+    borderRadius: wp(3),
+    paddingHorizontal: wp(2),
+  },
+  sendButtonContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: wp(5),
+    flex: 1,
+    backgroundColor: '#3F51B5',
+  },
+});
